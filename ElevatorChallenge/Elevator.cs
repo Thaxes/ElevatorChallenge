8118882a6c091a1c2bf9732be0beccb5f94d1e2e
Julia <jolinger@hotmail.com>
2023-08-23 15:37:09 -0400

    Adding and expanding on comments.

---
+++ ElevatorChallenge/Elevator.cs
@@ -0,0 +1,504 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge
+{
+    internal class Elevator
+    {
+    }
+    public class buttons
+    {
+        private int floor;
+        private bool direction;
+        public buttons(int floor, bool direction)
+        {
+            this.floor = floor;
+            this.direction = direction;
+        }
+        public int getFloor()
+        {
+            return floor;
+        }
+        public bool getDirection()
+        {
+            return direction;
+        }
+        public void setFloor(int floor)
+        {
+            this.floor = floor;
+        }
+        public void setDirection(bool direction)
+        {
+            this.direction = direction;
+        }
+        public void updateSensor(sensor elevatorSensor)
+        {
+            if (this.direction == true)
+            {
+                if (floor > 0 && floor < elevatorSensor.getFloorLimit())
+                    elevatorSensor.onButtonsUp(floor);  
+                elevatorSensor.onButtonsUp(floor);
+            }
+            else
+            {
+                if (floor > 0 && floor < elevatorSensor.getFloorLimit())
+                    elevatorSensor.onButtonsDown(floor);
+            }
+        }
+
+    }
+    public class sensor
+    {
+        private int floorLimit;
+        private int currentFloor;
+        private int destinationFloor;
+        private int weightLimit;
+        private int currentWeight;
+        private int direction;
+        private bool moving;
+        private bool[] buttonsUp;
+        private bool[] buttonsDown;
+        private bool[] elevatorButtons;
+        //Private constructor to place restrictions on the input
+        private sensor(int floorLimit, int currentFloor, int DestinationFloor, int weightLimit, int currentWeight, int direction, bool moving)
+        {
+            this.floorLimit = floorLimit;
+            this.currentFloor = currentFloor;
+            this.destinationFloor = DestinationFloor;
+            this.weightLimit = weightLimit;
+            this.currentWeight = currentWeight;
+            this.direction = direction;
+            this.moving = moving;
+            this.buttonsUp = new bool[floorLimit];
+            this.buttonsDown = new bool[floorLimit];
+            this.elevatorButtons = new bool[floorLimit];
+        }
+        //public static method to create a sensor object with restrictions on the input
+        public static sensor createSensor(int floorLimit, int currentFloor, int DestinationFloor, int weightLimit, int currentWeight, int direction, bool moving)
+        {
+            if (currentFloor > 0 && currentFloor < floorLimit && currentWeight < weightLimit)
+                return new sensor(floorLimit, currentFloor, DestinationFloor, weightLimit, currentWeight, direction, moving);
+            else
+            {
+                Console.WriteLine("Invalid input");
+                return null;
+            }
+        }
+        //because the functions are set up to only execute if conditions are met, call them all in a loop to execute whichever applies.
+        public void runElevatorForever(elevator elevator)
+        {
+            while (true)
+            {
+                //if going up, move up
+                moveElevatorUp();
+                //if going down, move down
+                moveElevatorDown();
+                //if going up and elevator is at up destination, stop
+                visitDestUp();
+                //if going down and elevator is at down destination, stop
+                visitDestDown();
+                //if elevator is at destination, stop
+                vistDestElevator();
+                //update the elevator
+                updateElevator(elevator);
+                //write information about what the elevator is doing
+                Console.WriteLine("Current Floor: " + currentFloor + " " + "Direction: " + direction + " " + "Destination floor:" + destinationFloor);
+            }
+        }
+        //Updates the direction of the elevator in all situations.
+        public void updtdateDirectionForever()
+        {
+            while(true)
+            {
+                //if moving up, update destination
+                updtDestWhileUpMoving();
+                //if moving down, update destination
+                updtDestWhileDownMoving();
+                //if still, update direction
+                updtDirectionUniAtRest();
+            }
+        }
+        //If the elevator is still and no buttons are pressed, it will scan for the closest floor with a button pressed.
+        private void updtDirectionUniAtRest()
+        {   
+
+            //evaluate if there are any buttons pressed
+            bool x = updtDestWhileUpStill();
+            bool y = updtDestWhileDownStill();
+            
+            
+            //if the above are false and elevator is not moving, start scanning for the nearest floor with a button pressed
+            if (x == false && y == false && moving == false)
+            {//repeat this for loop but in the opposite direction to check for every floor.
+                for (int i = currentFloor; i >= 1; i--)
+                {
+                    int j = currentFloor;
+                    if (j < floorLimit)
+                    {
+                        j++;
+                    }
+                    { //if button is pressed above, set destination and direction to up.
+                        if (ButtonsUp(i) == true)
+                        {
+                            direction = 1;
+                            destinationFloor = i;
+                            break;
+                        } //if button is pressed above for down, set direction to down and destination to that floor.
+                        else if (ButtonsDown(i) == true)
+                        {
+                            direction = 2;
+                            destinationFloor = i;
+                            break;
+                        } //if button is pressed below for Up, set direction to up and destination to that floor.
+                        else if (ButtonsUp(j) == true)
+                        {
+                            direction = 1;
+                            destinationFloor = j;
+                            break;
+                        } // if button is pressed below for down, set direction to down and destination to that floor.
+                        else if (ButtonsDown(j) == true)
+                        {
+                            direction = 2;
+                            destinationFloor = j;
+                            break;
+                        }
+                    }
+                } //repeat of above for loop to check in the opposite direction.
+                for (int i = currentFloor; i < floorLimit; i++)
+                {
+                    int j = currentFloor;
+                    if (j < floorLimit)
+                    {
+                        j++;
+                    }
+                    { //if button is pressed above, set destination and direction to up.
+                        if (ButtonsUp(i) == true)
+                        {
+                            direction = 1;
+                            destinationFloor = i;
+                            break;
+                        } //if button is pressed above for down, set direction to down and destination to that floor.
+                        else if (ButtonsDown(i) == true)
+                        {
+                            direction = 2;
+                            destinationFloor = i;
+                            break;
+                        } //if button is pressed below for Up, set direction to up and destination to that floor.
+                        else if (ButtonsUp(j) == true)
+                        {
+                            direction = 1;
+                            destinationFloor = j;
+                            break;
+                        } // if button is pressed below for down, set direction to down and destination to that floor.
+                        else if (ButtonsDown(j) == true)
+                        {
+                            direction = 2;
+                            destinationFloor = j;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        //if elevator is moving up, it will scan for the next floor with a button pressed. It will skip the very next floor because it is already going up. It will return true if it finds a button.
+        public bool updtDestWhileUpMoving()
+        {
+            if (moving == true && direction == 1)
+            {   //skip the next floor because it is unsafe to stop while in motion.
+                for (int i = currentFloor + 2; i < floorLimit; i++)
+                {
+                    if (i > floorLimit)
+                    {
+                        return false ;
+                    } //checks if someone wants to go up or stop on the way
+                    if (ButtonsUp(i) == true || ButtonsElevator(i) == true)
+                    {
+                        destinationFloor = i;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            return false;
+        }
+        //If the elevator is moving up, it will scan for the next floor with a button pressed. It will not skip the very next floor because it is not moving. It will return if it finds a button.
+        public bool updtDestWhileUpStill()
+        {
+            if (moving == false && direction ==1)
+            {
+                for (int i = currentFloor; i < floorLimit; i++)
+                {
+                    if (i > floorLimit)
+                    {
+                        return false;
+                    }   //checks if someone wants to go up or stop on the way
+                    if (ButtonsUp(i) == true || ButtonsElevator(i) == true)
+                    {
+                        destinationFloor = i;
+                        return true ;
+                    }
+                }
+                return false ;
+            }
+            return false;
+        }
+        //if the elevator is moving down, it will scan for the next floor with a button pressed. It will skip the very next floor because it is moving. It will return true if it finds a button.
+        public bool updtDestWhileDownMoving()
+        {
+            if (moving == true && direction == 2)
+            {   //skip the next floor because it is unsafe to stop while in motion.
+                for (int i = currentFloor - 2; i < floorLimit; i--)
+                {
+                    if (i < 1)
+                    {
+                        return false;
+                    }   //check if someone wants to go down or stop on the way
+                    if (ButtonsDown(i) == true || ButtonsElevator(i) == true)
+                    {
+                        destinationFloor = i;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            return false;
+        }
+        //if the elevator is moving down, it will scan for the next floor with a button pressed. It will not skip the very next floor because it is not moving. It will return true if it finds a button.
+        public bool updtDestWhileDownStill()
+        {
+            if (moving == false && direction == 2)
+            {
+                for (int i = currentFloor; i < floorLimit; i--)
+                {
+                    if (i < 1)
+                    {
+                        return false;
+                    }   //check is someone wants to go down or stop on the way.
+                    if (ButtonsDown(i) == true || ButtonsElevator(i) == true)
+                    {
+                        destinationFloor = i;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            return false;
+        }
+        //if current floor is destination floor and going in the correct direction, stop.
+        public void visitDestUp()
+        {   //if the elevator is not going down and the current floor is the destination floor, stop.
+            if (direction != 2 && currentFloor == destinationFloor)
+            {
+                setMoving(false);
+                offButtonsUp(currentFloor);
+                Console.WriteLine("All going Up, aboard!");
+                Thread.Sleep(1000);
+            }
+        }
+        //if current floor is destination floor and going in the correct direction, stop.
+        public void visitDestDown()      
+        {   //if the current direction is not up and this is the destination floor, stop.
+            if (direction != 1 && currentFloor == destinationFloor)
+            {
+                setMoving(false);
+                offButtonsDown(currentFloor);
+                Console.WriteLine("All going Down, aboard!");
+                Thread.Sleep(1000);
+            }
+        }
+        public void vistDestElevator()
+        {   //if the current floor is the destination floor set from inside the elevator, stop.
+            if (ButtonsElevator(currentFloor) == true)
+            {
+                setMoving(false);
+                offElevatorButtons(currentFloor);
+                Console.WriteLine("This is a stop for floor: " + currentFloor);
+                Thread.Sleep(1000);
+            }
+        }
+        //move the elevator one floor up. Wait three seconds.
+        public void moveElevatorUp()
+        {
+           if (currentFloor < destinationFloor)
+            {
+                setMoving(true);
+                Thread.Sleep(3000);
+                currentFloor++;
+            }   
+        }
+        //Move the elevator one floor down. Wait three seconds.
+        public void moveElevatorDown()
+        {
+            if (currentFloor > destinationFloor)
+            {
+                setMoving(true);
+                Thread.Sleep(3000);
+                currentFloor--;
+            }
+        }
+        //Update the elevator's onboard data.
+        public void updateElevator(elevator elevator)
+        {
+            elevator.setCurrentFloor(currentFloor);
+            elevator.setDestinationFloor(destinationFloor);
+            elevator.setCurrentWeight(currentWeight);
+            elevator.setDirection(direction);
+            elevator.setMoving(moving);
+        }
+        //return true if the floor is flagged to stop at.
+        public bool ButtonsUp(int floor)
+        {
+            return buttonsUp[floor - 1];
+        }
+        //return true if the floor is flagged to stop at.
+        public bool ButtonsDown(int floor)
+        {
+            return buttonsDown[floor - 1];
+        }
+
+        public bool ButtonsElevator(int floor)
+        {
+            return elevatorButtons[floor - 1];
+        }
+        public int getFloorLimit()
+        {
+            return floorLimit;
+        }
+        public int getCurrentFloor()
+        {
+            return currentFloor;
+        }
+        public int getDestinationFloor()
+        {
+            return destinationFloor;
+        }
+        public int getWeightLimit()
+        {
+            return weightLimit;
+        }
+        public int getCurrentWeight()
+        {
+            return currentWeight;
+        }
+        //getDirection 0 if stopped, 1 if up, 2 if down
+        public int getDirection()
+        {
+            return direction;
+        }
+        public bool getMoving()
+        {
+            return moving;
+        }
+        public void onButtonsUp(int floor)
+        {
+            buttonsUp[floor -1] = true;
+        }
+        public void onButtonsDown(int floor)
+        {
+            buttonsDown[floor - 1] = true;
+        }
+        public void onElevatorButtons(int floor)
+        {
+            elevatorButtons[floor - 1] = true;
+        }
+        public void offButtonsUp(int floor)
+        {
+            buttonsUp[floor - 1] = false;
+        }
+        public void offButtonsDown(int floor)
+        {
+            buttonsDown[floor - 1] = false;
+        }
+        public void offElevatorButtons(int floor)
+        {
+            elevatorButtons[floor - 1] = false;
+        }
+        public void setFloorLimit(int floorLimit)
+        {
+            this.floorLimit = floorLimit;
+        }
+        public void setCurrentFloor(int currentFloor)
+        {
+            this.currentFloor = currentFloor;
+        }
+        public void setDestinationFloor(int DestinationFloor)
+        {
+            this.destinationFloor = DestinationFloor;
+        }
+        public void setWeightLimit(int weightLimit)
+        {
+            this.weightLimit = weightLimit;
+        }
+        public void setCurrentWeight(int currentWeight)
+        {
+            this.currentWeight = currentWeight;
+        }
+        public void setDirection(int direction)
+        {
+            this.direction = direction;
+        }
+        public void setMoving(bool moving)
+        {
+            this.moving = moving;
+        }
+
+
+    }
+    public class elevator
+    {
+        int currentFloor;
+        int destinationFloor;
+        int direction;
+        bool moving;
+
+        public elevator(int currentFloor, int destinationFloor, int direction, bool moving)
+        {
+            this.currentFloor = currentFloor;
+            this.destinationFloor = destinationFloor;
+            this.direction = direction;
+            this.moving = moving;
+        }
+
+        public int getCurrentFloor()
+        {
+            return currentFloor;
+        }
+        public int getDestinationFloor()
+        {
+            return destinationFloor;
+        }
+        public int getDirection()
+        {
+            return direction;
+        }
+        public bool getMoving()
+        {
+            return moving;
+        }
+        public void setCurrentFloor(int currentFloor)
+        {
+            this.currentFloor = currentFloor;
+        }
+        public void setDestinationFloor(int destinationFloor)
+        {
+            this.destinationFloor = destinationFloor;
+        }
+        public void setDirection(int direction)
+        {
+            this.direction = direction;
+        }
+        public void setMoving(bool moving)
+        {
+            this.moving = moving;
+        }
+
+        internal void setCurrentWeight(int currentWeight)
+        {
+            this.currentFloor = currentWeight;
+        }
+    }
+}
---
+++ ElevatorChallenge/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using ElevatorChallenge;
+
+bool quit = false;
+buttons elevatorButtons = new buttons(1,true);
+sensor elevatorSensor = sensor.createSensor(10, 1, 1, 1000, 1, 1, false);
+elevator elevator = new elevator(1,1,0,false);
+Thread thread = new Thread(() => elevatorSensor.updtdateDirectionForever());
+thread.Start();
+Thread thread1 = new Thread(() => elevatorSensor.runElevatorForever(elevator));
+thread1.Start();
+
+readInput(elevatorButtons);
+
+
+//This method reads user input and updates the button and sensor objects accordingly. The sensorScan method updates the destination floor.
+ void readInput(buttons elevatorButtons)
+{
+    string alphaPart = "t";
+    do
+    {
+        //create a regex expression to separate the floor number from the direction.
+        Regex re = new Regex(@"(\d+)([u,U,d,D,q,Q]+)");
+        Console.WriteLine("Enter the floor number and direction, denoted as U or D, without spaces: ");
+        //Save user input to a variable
+        var floorDirection = Console.ReadLine();
+        //evaluate if the user input follows the regex expression
+        Match result = re.Match(floorDirection);
+        //if the user input does not follow the regex expression, prompt the user to re-enter the input
+        while (!result.Success)
+        {
+            Console.WriteLine("Invalid input. Please enter the floor number and direction, denoted as U or D, without spaces: ");
+            floorDirection = Console.ReadLine();
+            result = re.Match(floorDirection);
+        }
+        //if the user input follows the regex expression, save the floor number and direction to the button object
+        if (result.Success)
+        {
+            alphaPart = result.Groups[2].Value;
+            int numericPart = Int32.Parse(result.Groups[1].Value);
+
+            if (alphaPart == "u" || alphaPart == "U")
+            {
+                elevatorButtons.setDirection(true);
+                elevatorButtons.setFloor(numericPart);
+                Console.WriteLine("Floor: " + elevatorButtons.getFloor() + " Direction: " + elevatorButtons.getDirection());
+            }
+            else if (alphaPart == "D" || alphaPart == "d")
+            {
+                elevatorButtons.setDirection(false);
+                elevatorButtons.setFloor(numericPart);
+                Console.WriteLine("Floor: " + elevatorButtons.getFloor() + " Direction: " + elevatorButtons.getDirection());
+            }
+            else if (alphaPart == "Q" || alphaPart == "q")
+            {
+                quit = true;
+                break;
+            }
+            elevatorButtons.updateSensor(elevatorSensor);
+            //elevatorSensor.sensorScan();
+            Console.WriteLine("Current Floor: " + elevatorSensor.getCurrentFloor() + " Destination Floor: " + elevatorSensor.getDestinationFloor() + " Direction: " + elevatorSensor.getDirection() + " Moving: " + elevatorSensor.getMoving());
+        }
+    } while (alphaPart != "Q" || alphaPart != "q");
+}
